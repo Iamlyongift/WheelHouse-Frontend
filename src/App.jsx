3dfbe65d9c74353923f70bee33b5f9f51d5facbf
iamlyongift <chukwumagift6036@gmail.com>
2024-09-14 11:19:39 +0100

    added preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
-import About from "./components/pages/About";
-import Product from "./components/pages/Product";
-import Feature from "./components/pages/Feature";
-import Home from "./components/Home/Home";
-import Register from "./components/pages/Register";
 import Footer from "./components/pages/Footer";
-import LoginPage from "./components/pages/LoginPage";
+import Loader from "./components/Loader/Loader";
+
+// Lazy load your components
+const About = lazy(() => import("./components/pages/About"));
+const Product = lazy(() => import("./components/pages/Product"));
+const Feature = lazy(() => import("./components/pages/Feature"));
+const Home = lazy(() => import("./components/Home/Home"));
+const Register = lazy(() => import("./components/pages/Register"));
+const LoginPage = lazy(() => import("./components/pages/LoginPage"));
 
 function App() {
   const current_theme = localStorage.getItem("current_theme");
@@ -16,20 +19,23 @@ function App() {
   useEffect(() => {
     localStorage.setItem("current_theme", theme);
   }, [theme]);
+
   return (
     <Router>
       <div className={`container ${theme}`}>
-        <Navbar theme={theme} setTheme={setTheme} />8
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/feature" element={<Feature />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <Navbar theme={theme} setTheme={setTheme} />
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/feature" element={<Feature />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Suspense>
       </div>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,20 @@
+import { DNA} from "react-loader-spinner";
+import "../Css/Loader.css"; // Import the CSS file
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+<DNA
+  visible={true}
+  height="80"
+  width="80"
+  ariaLabel="dna-loading"
+  wrapperStyle={{}}
+  wrapperClass="dna-wrapper"
+  />
+
+    </div>
+  );
+};
+
+export default Loader;
