94f7f552a57db51c425ceb8b4ea0937458090708
iamlyongift <chukwumagift6036@gmail.com>
2024-11-20 21:37:50 +0100

    modified viteConfig file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://wheelhouse.onrender.com",
+      "/api": "https://api.cribsandrides.com",
     },
   },
   plugins: [react()],
